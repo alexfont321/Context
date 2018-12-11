95977172f023f15f42f17ecbc2fafe6266a00683
unknown <alexfont321@gmail.com>
2018-12-11 15:53:06 -0600

    first commit: project database functioning

---
+++ Context/Models/Book.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Context.Models
+{
+    public class Book
+    {
+        [Key]
+        public int BookId { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string AuthorFirstName { get; set; }
+
+        [Required]
+        public string AuthorLastName { get; set; }
+
+        [Required]
+        public string Country { get; set; }
+
+        [Required]
+        public string HistoricalLink { get; set; }
+
+        public string AuthorInfo { get; set; }
+
+        public string BookInfo { get; set; }
+
+        public string CountryInfo { get; set; }
+
+        public string HistoricalInfo { get; set; }
+
+        public virtual ICollection<UserBook> UserBooks { get; set; }
+
+
+
+    }
+}
---
+++ Context/Models/UserBook.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Context.Models
+{
+    public class UserBook
+    {
+
+        [Key]
+        public string UserBookId { get; set; }
+
+        [Required]
+        public int BookId { get; set; }
+
+        [Required]
+        public int UserId { get; set; }
+
+        public Book Book { get; set; }
+
+        public ApplicationUser User { get; set; }
+
+    }
+}
